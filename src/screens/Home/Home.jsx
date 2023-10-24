906f2fe28627d87603fc771f120788f868a370b7
Uriel Milgron <urielmilgron18@gmail.com>
2023-10-24 20:52:32 -0300

    CreatePost component added on header of flatlist in Home Screen

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, ScrollView } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import styles from './Home.style'
 import data from '../../data/posts'
@@ -6,17 +6,20 @@ import PublicationItem from './components/PublicationItem'
 import {CreatePostInput} from '../../components/'
 
 const Home = () => {
+  const headerFlatList = () => {
+    return <CreatePostInput/>
+  }
   return (
-    <ScrollView contentContainerStyle={{flexGrow: 1}} style={styles.container}>
-      <CreatePostInput/>
+    <View style={styles.container}>
       <FlatList
+      ListHeaderComponent={headerFlatList}
       data={data}
       keyExtractor={(post) => post.id}
       renderItem={({item}) => (
         <PublicationItem publication={item}/>
       )}
       />
-    </ScrollView>
+    </View>
   )
 }
 
